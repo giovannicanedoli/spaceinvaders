cb9c0571969a931edcb5024f75cb01ace2a86a34
Gio <gio.canedoli02@gmail.com>
2024-01-04 23:14:40 +0100

    updates

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -13,7 +13,12 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class GameScreen implements Screen {
     final MainGame game;
     private Texture spaceImage;
+    private Texture enemyImage;
+    private Texture laserImage;
+
     private Rectangle spaceShip;
+    private Rectangle enemy;
+    private Rectangle laser;
 
     OrthographicCamera camera;
 
@@ -21,20 +26,35 @@ public class GameScreen implements Screen {
     private final int HEIGHT = 1080;
 
     private final int SPEED = 400;
+
+    //private int nenemy = 0;
+
     public GameScreen(final MainGame game) {
         this.game = game;
         spaceImage = new Texture(Gdx.files.internal("ship.png"));
+        laserImage = new Texture(Gdx.files.internal("pixel_laser_red.png"));
+        enemyImage = new Texture(Gdx.files.internal("pixel_ship_red_small.png"));
+
         spaceShip = new Rectangle();
+        enemy = new Rectangle();
+        laser = new Rectangle();
+
         camera = new OrthographicCamera();
         camera.setToOrtho(false, WIDTH, HEIGHT);
 
         //asset non si centra correttamente, che fare?
-        spaceShip.x = 800/2 - 64/2;
+        spaceShip.x = WIDTH/2 - 10;
         spaceShip.y = 20;
 
+        enemy.x = (int)(Math.random() * (WIDTH - 20 + 1)) + 10;
+        enemy.y = (int)(Math.random() * (HEIGHT - 20 - 500 + 1)) + 500;
+
+        laser.x = spaceShip.x + 10;
+        laser.y = spaceShip.y;
+
         //perché non è centrato??
-        spaceShip.width = 64;
-        spaceShip.height = 64;
+        spaceShip.width = 20;
+        spaceShip.height = 20;
     }
 
     @Override
@@ -53,39 +73,47 @@ public class GameScreen implements Screen {
 
         game.batch.begin();
         game.batch.draw(spaceImage, spaceShip.x, spaceShip.y);
+        //if(nenemy != 5){
+            //random x and y
+        game.batch.draw(enemyImage, enemy.x, enemy.y);
+
+        game.batch.draw(enemyImage, enemy.x, enemy.y);
+
+
         game.batch.end();
 
-        if(Gdx.input.isKeyPressed(Input.Keys.SPACE)){
-            if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+        if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT)){
+            if(Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A)){
                 spaceShip.x -= 2*SPEED * Gdx.graphics.getDeltaTime();
             }
-            if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D)){
                 spaceShip.x += 2*SPEED * Gdx.graphics.getDeltaTime();
             }
-            if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+            if(Gdx.input.isKeyPressed(Input.Keys.UP) || Gdx.input.isKeyPressed(Input.Keys.W)){
                 spaceShip.y += 2*SPEED * Gdx.graphics.getDeltaTime();
             }
-            if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+            if(Gdx.input.isKeyPressed(Input.Keys.DOWN) || Gdx.input.isKeyPressed(Input.Keys.S)){
                 spaceShip.y -= 2*SPEED * Gdx.graphics.getDeltaTime();
             }
         }else{
-            if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            if(Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A)){
                 spaceShip.x -= SPEED * Gdx.graphics.getDeltaTime();
             }
-            if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D)){
                 spaceShip.x += SPEED * Gdx.graphics.getDeltaTime();
             }
-            if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+            if(Gdx.input.isKeyPressed(Input.Keys.UP) || Gdx.input.isKeyPressed(Input.Keys.W)){
                 spaceShip.y += SPEED * Gdx.graphics.getDeltaTime();
             }
-            if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+            if(Gdx.input.isKeyPressed(Input.Keys.DOWN) || Gdx.input.isKeyPressed(Input.Keys.S)){
                 spaceShip.y -= SPEED * Gdx.graphics.getDeltaTime();
             }
         }
 
-
         if(spaceShip.x < 0) spaceShip.x = 0;
+        if(spaceShip.y < 0) spaceShip.y = 0;
         if(spaceShip.x > WIDTH - spaceShip.width) spaceShip.x = 0;
+        if(spaceShip.y > HEIGHT - spaceShip.height ) spaceShip.y = 0;
 
     }
 
@@ -112,5 +140,7 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
         spaceImage.dispose();
+        enemyImage.dispose();
+        laserImage.dispose();
     }
 }
